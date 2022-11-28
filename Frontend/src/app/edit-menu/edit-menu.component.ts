0a1c8e6c9d33e2e505c8a12bcf290120f7ab66ee
Sharns <oskarsharns@gmail.com>
2022-11-28 21:50:46 +0200

    revert5

--- Frontend/src/app/edit-menu/edit-menu.component.ts
+++ Frontend/src/app/edit-menu/edit-menu.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { TriviaGameComponent } from '../trivia-game/trivia-game.component';
 
 @Component({
   selector: 'app-edit-menu',
   templateUrl: './edit-menu.component.html',
   styleUrls: ['./edit-menu.component.css']
+
 })
 export class EditMenuComponent {
+  constructor(private http: HttpClient, private router: Router) { }
+  posts: any[] = [];
+  loadPosts() {
+    this.http
+      .get('https://localhost:7151/api/TriviaGame/CreateQuestion', {})
+      .subscribe((posts: any) => {
+        this.posts = posts;
+      });
+  }
+  createPost() {
+    this.http.post('https://localhost:7151/api/TriviaGame/CreateQuestion', {
+      // title: 'Hello world',
+      // body: 'This is body',
+    }).subscribe(
+      (response: any) => {
+        alert(JSON.stringify(response));
+      },
+      (error) => {
+        alert(JSON.stringify(error));
+      }
+    );
+  }
 
+  btnClick() {
+    // this.router.navigateByUrl('trivia-game');
+    this.router.navigateByUrl('/trivia-game');
+  }
 }
