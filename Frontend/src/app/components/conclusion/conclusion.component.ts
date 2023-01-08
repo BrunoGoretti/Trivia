20c0f6ecd3c54bd14d49832dde96aab45bbea8fc
BrunoGoretti <104521859+BrunoGoretti@users.noreply.github.com>
2023-01-09 01:15:56 +0400

    Revert "Delete conclusion"
    This reverts commit 7afac52db50a8df906e19ab024009384f9bc34db.

---
+++ Frontend/src/app/components/conclusion/conclusion.component.ts
@@ -0,0 +1,9 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-conclusion',
+  templateUrl: './conclusion.component.html',
+  styleUrls: ['./conclusion.component.css']
+})
+export class ConclusionComponent {
+}
